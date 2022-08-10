de97c6b63185cf64c967fdfc67bdcebace641438
Alabama Whiskey <amwtablet@gmail.com>
2022-08-09 23:22:00 -0400

    adding logic for options and composition apis

--- src/views/Composition.vue
+++ src/views/Composition.vue
@@ -1,25 +1,138 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <h3>You Have -- To-Do Items on Your List</h3>
+    <h3>You Have {{ todosCount }} To-Do Items on Your List</h3>
     <div>
-      <input placeholder="Add To-Do" type="text">
+      <input 
+        v-model="data.newTodoName"
+        @keyup.enter="addTodo"
+        placeholder="Add To-Do"
+        type="text"
+        >
     </div>
     <div>
       <ul>
-        <li>
-          <span>Item One</span>
-          <button>Delete</button>
+        <li v-for="(todo, i) in data.todos" :key="todo.id">
+          <span>{{ todo.name }}</span>
+          <button @click="deleteTodo(i)">Delete</button>
         </li>
-        <li>
-          <span>Item Two</span>
-          <button>Delete</button>
-        </li>
-        <li>
-          <span>Item Three</span>
-          <button>Delete</button>
-        </li>
+
      </ul>
     </div>
   </div>
 </template>
+
+<script>
+import { ref, reactive, computed } from 'vue'
+
+export default {
+    setup() {
+      let newTodoName = ref('')
+      let todos = ref([])
+      
+      let data = reactive({
+        newTodoName: 'Add a To-Do',
+        todos: [
+        {
+          id: 1,
+          name: 'Item One'
+        },
+        {
+          id: 2,
+          name: 'Item Two'
+        },
+        {
+          id: 3,
+          name: 'Item Three'
+        }
+        ]
+      })
+
+      let todosCount = computed(() => {
+        return todos.value.length
+      })
+     
+     function addTodo() {
+      let newTodo = {
+        //Date used as random ID
+        id: Date.now(),
+        name: newTodoName.value
+      }
+      todos.value.push(newTodo);
+      //removes entry upon enter
+      newTodoName.value = ''
+     }
+
+    function deleteTodo(i){
+      todos.value.splice(i, 1)
+    }
+
+    return {
+      newTodoName,
+      todos,
+      todosCount,
+      data,
+      addTodo,
+      deleteTodo
+    }
+  }
+}
+</script>
+
+<style>
+  ::placeholder { /* Chromium, Firefox, Opera, Safari 10.1+ */
+    color: #f52789;
+    opacity: 1; /* Firefox */
+    margin-left: px;
+  }
+
+  input {
+    color:#f52789;
+    background-color: transparent;
+    border: 1px solid #f52789;
+    border-radius: 5px;
+    height: 30px;
+  }
+  
+  input:hover {
+    border: 1px solid #f569ad;
+  }
+
+
+ ul {
+  list-style: none;
+  padding: 0;
+  width: 200px;
+  margin: 20px auto 0;
+ }
+
+ li {
+  display: flex;
+  margin-top: 5px;
+  margin-bottom: 10px;
+ }
+
+ li span {
+  flex-grow: 1;
+  border: 1px solid #1685f8;
+  border-radius: 5px;
+ }
+
+ li span:hover {
+    border: 1px solid #63abf7;
+ }
+
+ li button {
+  border-radius: 10px;
+  margin-left: 10px;
+  background-color: transparent;
+  font-weight: bold;
+  color: #f52789;
+  border: 1px solid #f52789;
+ }
+
+ li button:hover{
+  background-color: #f569ad;
+  color: white;
+ }
+</style>
--- src/views/Options.vue
+++ src/views/Options.vue
@@ -1,24 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <h3>You Have -- To-Do Items on Your List</h3>
+    <h3>You Have {{ todosCount }} To-Do Items on Your List</h3>
     <div>
-      <input placeholder="Add To-Do" type="text">
+      <input 
+        v-model="newTodoName"
+        @keyup.enter="addTodo"
+        placeholder="Add To-Do"
+        type="text">
     </div>
     <div>
       <ul>
-        <li>
-          <span>Item One</span>
-          <button>Delete</button>
+        <li v-for="(todo, i) in todos" :key="todo.id">
+          <span>{{ todo.name }}</span>
+          <button @click="deleteTodo(i)">Delete</button>
         </li>
-        <li>
-          <span>Item Two</span>
-          <button>Delete</button>
-        </li>
-        <li>
-          <span>Item Three</span>
-          <button>Delete</button>
-        </li>
+
      </ul>
     </div>
   </div>
@@ -26,7 +23,47 @@
 
 <script>
 export default {
-  name: 'Options',
+  //data, methods, and computed are all OPTIONS
+  data() {
+    return {
+      newTodoName: 'Add a To-Do',
+      todos: [
+        {
+          id: 1,
+          name: 'Item One'
+        },
+        {
+          id: 2,
+          name: 'Item Two'
+        },
+        {
+          id: 3,
+          name: 'Item Three'
+        }
+      ]
+    }
+  },
+  computed: {
+    todosCount(){
+      return this.todos.length
+    }
+  },
+  methods: {
+    addTodo() {
+      let newTodo = {
+        //Date used as random ID
+        id: Date.now(),
+        name: this.newTodoName
+      }
+      this.todos.push(newTodo);
+      //removes entry upon enter
+      this.newTodoName = ''
+    },
+    deleteTodo(i) {
+      //second parameter indicates # of objects to splice
+      this.todos.splice(i, 1)
+    }
+  }
 }
 </script>
 
@@ -43,7 +80,10 @@ export default {
     border: 1px solid #f52789;
     border-radius: 5px;
     height: 30px;
-
+  }
+  
+  input:hover {
+    border: 1px solid #f569ad;
   }
 
 
@@ -62,15 +102,25 @@ export default {
 
  li span {
   flex-grow: 1;
-  border: 1px solid;
+  border: 1px solid #1685f8;
   border-radius: 5px;
+ }
+
+ li span:hover {
+    border: 1px solid #63abf7;
  }
 
  li button {
   border-radius: 10px;
   margin-left: 10px;
   background-color: transparent;
+  font-weight: bold;
   color: #f52789;
   border: 1px solid #f52789;
+ }
+
+ li button:hover{
+  background-color: #f569ad;
+  color: white;
  }
 </style>
